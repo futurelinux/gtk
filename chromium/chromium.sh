1a1013d76843fcb909e1b75473be09c6f51c8446
AlmAck <gluca86@gmail.com>
2013-04-28 20:49:39 +0200

    Move chromium to extra

---
+++ chromium/chromium.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Override command-line options
+# Based on Gentoo's chromium package (and by extension, Debian's)
+if [[ -f /extra/etc/chromium/default ]]; then
+	. /extra/etc/chromium/default
+fi
+
+# Prefer user defined CHROMIUM_USER_FLAGS (from env) over system
+# default CHROMIUM_FLAGS (from /etc/chromium/default)
+CHROMIUM_FLAGS=${CHROMIUM_USER_FLAGS:-$CHROMIUM_FLAGS}
+
+export CHROME_WRAPPER=$(readlink -f "$0")
+export CHROME_DESKTOP=chromium.desktop
+
+exec /extra/usr/lib/chromium/chromium $CHROMIUM_FLAGS "$@"
+
