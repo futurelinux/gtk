3ff528529a665c7c34bd2fafed3bf9b97ca4a8e1
Weng Xuetian <wengxt@gmail.com>
2014-06-13 00:28:45 +0000

    fix duckduckgo default search engine

--- firefox-kde/vendor.js
+++ firefox-kde/vendor.js
@@ -4,6 +4,7 @@ pref("intl.locale.matchOS", true);
 // Enable default browser checking.
 pref("browser.shell.checkDefaultBrowser", true);
 pref("browser.search.defaultenginename", "DuckDuckGo");
+pref("browser.search.order.extra.duckduckgo", "DuckDuckGo");
 pref("browser.search.selectedEngine", "DuckDuckGo");
 
 // Don't disable our bundled extensions in the application directory
