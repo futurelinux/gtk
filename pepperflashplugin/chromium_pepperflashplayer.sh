3726d29487e800443f880ccf59271dcada54f81f
AlmAck <gluca86@gmail.com>
2015-01-11 01:17:47 +0100

    pepperflashplugin first PKGBUILD

---
+++ pepperflashplugin/chromium_pepperflashplayer.sh
@@ -0,0 +1,6 @@
+# Include this script from /etc/chromium-browser/default to enable
+# the Adobe/Google Pepper Flash plugin.
+
+FLASH_VERSION=@FLASH_VERSION@
+
+CHROMIUM_FLAGS="$CHROMIUM_FLAGS --ppapi-flash-path=/usr/lib/pepflashplugin-installer/libpepflashplayer.so --ppapi-flash-version=$FLASH_VERSION"
