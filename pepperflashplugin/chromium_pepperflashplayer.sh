a654b665feaa759f16f443f493eb3f9b60b0193a
Chaoting Liu <brli@chakralinux.org>
2016-11-02 09:50:49 +0000

    ppflash: pkgrel++, add back chromium flag file
    - the chromium flag file is now installed by default to /etc/chromium
    - we're diverged from Arch as they're using customized launcher while we
    still based ours on gentoo and Debian, as the legacy one provided by
    Arch

---
+++ pepperflashplugin/chromium_pepperflashplayer.sh
@@ -0,0 +1,7 @@
+# Include this script from /etc/chromium-browser/default to enable
+# the Adobe/Google Pepper Flash plugin.
+
+FLASH_VERSION=@FLASH_VERSION@
+
+CHROMIUM_FLAGS="$CHROMIUM_FLAGS --ppapi-flash-path=/usr/lib/PepperFlash/libpepflashplayer.so \
+--ppapi-flash-version=$FLASH_VERSION"
