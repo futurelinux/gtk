3c532d2905360aac226c54ab043cde201b9f89c9
AlmAck <gluca86@gmail.com>
2018-01-07 17:07:19 +0100

    let's use the chakra-gitlab-ci-scripts repository to retrieve the scripts
    with this method we can modify the scripts and run again the build process without generating a new commit.
    
    the user inizialization is now part of chakralinux/makepkg
    [skip-ci]

--- .build-bin/import-validpgpkeys.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$( dirname $( readlink -e $0 ) )
-source "$SCRIPT_DIR/../.build-lib/ci-library.sh"
-
-_do list_packages
-
-
-# `gpg --recv-key` requires write access to the current user's home directory!
-_do gpg --recv-key $(get_validpgpkeys)
